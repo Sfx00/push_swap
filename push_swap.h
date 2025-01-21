6b506108f3ccd0d9073a5bc6247bb7b1cea67250
SFX <oussamaba348@gmail.com>
2025-01-21 01:14:10 +0100

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+typedef struct s_list
+{
+    int number;
+    struct s_list *next;
+}t_list;
+
+
+
+
+
+#endif
