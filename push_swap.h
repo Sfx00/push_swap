c132401ee4e933a5703042c0391bc46658b59d84
SFX <oussamaba348@gmail.com>
2025-01-22 22:33:35 +0100

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -12,10 +12,16 @@ typedef struct s_list
 {
     int number;
     struct s_list *next;
+    int level;
 }t_list;
 
+void sa(t_list **stack);
+void ra(t_list **stack_A);
+void rra(t_list **stack_A);
 
 
+void	*ft_calloc(size_t nmemb, size_t size);
+t_list *create_node(int nb);
 
 
 #endif
